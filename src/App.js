3cab397945a5da6da398635cb2efcc3b3d9ed56f
To-ya Yamanishi <neotyhdpc@gmail.com>
2020-06-24 23:09:56 +0900

    implrment component using multiple states

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import React, { useState } from "react";
 
-const App = () => {
-  const [count, setCount] = useState(0);
-  const increment = () => {
-    setCount(count + 1);
-  };
+const App = (props) => {
+  const [name, setName] = useState(props.name);
+  const [price, setPrice] = useState(props.price);
 
   return (
-    <>
-      <div>count:{count}</div>
-      <button onClick={increment}>+1</button>
-    </>
+    <div>
+      <p>
+        現在の{name}は、{price}円です。
+      </p>
+      <button onClick={() => setPrice(price + 1)}>+1</button>
+      <input value= {name} onChange={e => setName(e.target.value)}/>
+    </div>
   );
+};
+
+App.defaultProps = {
+  name: "",
+  price: 1000,
 };
 
 export default App;
